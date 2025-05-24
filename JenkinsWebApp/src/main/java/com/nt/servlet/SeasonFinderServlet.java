ef80064f95656cd2bf8200db01cd1a82f7bdd15c
thotibharadwaj <thotibharadwaj@gmail.com>
2025-05-24 16:01:45 +0530

    first commit

---
+++ JenkinsWebApp/src/main/java/com/nt/servlet/SeasonFinderServlet.java
@@ -0,0 +1,60 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/seasonurl")
+public class SeasonFinderServlet extends HttpServlet{
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//set the content type
+		resp.setContentType("text/html");
+		
+		//get PrintWriter
+		PrintWriter pw = resp.getWriter();
+		
+		//get system time
+		LocalDateTime idt = LocalDateTime.now();
+		
+		//get current month value
+		int month = idt.getMonthValue();
+
+		//get season
+		String seasonName = null ;
+		if(month >=3 && month <=6) {
+			seasonName = "Summer Season";
+		}
+		else if(month >6 && month>=11) {
+			seasonName = "Rainy Season";
+		}
+		else {
+			seasonName = "Winter Season";
+		}
+		
+		//write result / output to the browser 
+		pw.println("<h1 style='color:green ; text-align:center'>" + seasonName +"</h1>");
+		
+		//add home hyperlink  	
+		pw.println("<h1 style='text-align:center' ><a href='index.jsp' > Home <img src='images/home.jpeg' width='50' height='50'></a></h1>");
+		
+		//close stream
+		pw.close();
+		
+		
+		
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+}
---
+++ JenkinsWebApp/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+
+
+<h1 style="color:red; text-align:center "> HTML to Servlet Communication</h1>
+
+<br><br>
+
+<h1> <a href="seasonurl">Show Season Name</a></h1>
